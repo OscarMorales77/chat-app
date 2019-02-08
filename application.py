27116386251373545c5fbb865624d39ff1d7169d
Oscar <NAge@email.com>
2019-02-07 20:06:51 -0600

    Send and Receive protocols are working.

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask, render_template, request
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, join_room, leave_room
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
@@ -13,6 +13,7 @@ channel_list = []
 
 @app.route("/")
 def index():
+    print("workingx")
     return render_template("index.html")
 
 
@@ -27,6 +28,17 @@ def channel(name):
     channel_list.append(name) #iff the user clicks on the link, then the server will save the channel
     return render_template("channel.html", channel_name=name)
 
+
+#this will accept a json object
+@socketio.on("server message")
+def vote(data):
+    print("-------->before selection")
+    selection = data["message"]
+    
+    print("------------>after selection")
+    print("you message from client "+selection)
+  
+    emit("client message", {"serverOut": selection}, broadcast=True,)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
--- static/channel.js
+++ static/channel.js
@@ -1,5 +1,58 @@
 document.addEventListener('DOMContentLoaded', function () {
-    
+
     localStorage.setItem('lastChannel', document.URL);
-    
+
+    document.querySelector('span').innerHTML = `Hi, ${localStorage.getItem('userName')}`;
+
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+    console.log("your out is " + location.protocol + '//' + document.domain + ':' + location.port);
+
+    // the callback function for socket.on is the lamda function  that is called once the socket is connected
+    //further, there is another lamnda function "onsubmit"
+    socket.on('connect', () => {
+        console.log("working ");
+        
+        var message;
+        document.querySelector('#messageForm').onsubmit = () => {
+            message = document.querySelector('#formInput').value;
+            console.log(message);
+           socket.emit('server message', { 'message': message });
+            return false;
+        }
+
+
+
+    });
+
+    console.log("does not wait")
+    /*
+    document.querySelector('#messageForm').onsubmit = () => {
+        var message = document.querySelector('#formInput').value;
+        console.log(message);
+        socket.on('connect', () =>  {
+            console.log("working "+message)
+            socket.emit('server message', { 'message': message });
+        });
+
+
+        return false;
+    }
+
+      // When  the server emmits a message, add to the unordered list
+      socket.on('client message', data => {
+        const li = document.createElement('li');
+        li.innerHTML = `Your mesage is: ${data.serverOut}`;
+        document.querySelector('#messages').append(li);
+    });
+
+    */
+
+    socket.on('client message', data => {
+        console.log(data.room)
+        const li = document.createElement('li');
+        li.innerHTML = `Your mesage is: ${data.serverOut}`;
+        document.querySelector('#messages').append(li);
+    });
+
+
 });
