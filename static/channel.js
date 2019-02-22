97757b57905a6d4306e9032afa5cae6e43515aab
Oscar <NAge@email.com>
2019-02-21 19:27:55 -0600

    Code refactoring for channel.js

--- static/channel.js
+++ static/channel.js
@@ -1,70 +1,69 @@
+var recepient = 'all';
 document.addEventListener('DOMContentLoaded', function () {
-    localStorage.setItem('lastChannel', document.URL);
-    //send a ajax request to the server when the user visits the channel for the first time
-    const request = new XMLHttpRequest();
-    request.open('POST', location.pathname);
-    const data = new FormData();
-    data.append('userName', localStorage.getItem('userName'));
-    request.send(data);
-    document.querySelector('span').innerHTML = `Hi, ${localStorage.getItem('userName')}`;
 
+    setAttributes();
+  
+    createButtons();
 
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
-    console.log("your out is " + location.protocol + '//' + document.domain + ':' + location.port);
-    var recepient = 'all'
-    console.log('current recpepient  ' + recepient);
-
-    function createButtons() {
-        document.querySelectorAll('.currentUsers').forEach(button => {
-            button.onclick = () => {
-                recepient = button.dataset.user;
-                console.log(recepient);
-            };
-        });
-    }
-    createButtons();
-    // the callback function for socket.on is the lamda function  that is called once the socket is connected
-    //further, there is another lamnda function "onsubmit"
+ 
     socket.on('connect', () => {
-        console.log("working ");
         //join a "room" with the unique location.pathname (JS method) name
-        socket.emit('join', { 'room': location.pathname, 'user': localStorage.getItem('userName')}); // notice how we are calling emit(); right away
-        var message;
+         // Join a room immediately after establishing connection to the socket
+        socket.emit('join', { 'room': location.pathname, 'user': localStorage.getItem('userName') });
+        let message;
         document.querySelector('#messageForm').onsubmit = () => {
             message = document.querySelector('#formInput').value;
             if (recepient === 'all') {
+                console.log("too all")
+                console.log("current recepient is "+ recepient)
                 socket.emit('server message', { 'message': message, 'room': location.pathname });
                 return false;
             } else {
-                socket.emit('private message', { 'message': message, 'recepient': recepient});
+                console.log("privae message")
+                socket.emit('private message', { 'message': message, 'recepient': recepient });
                 return false;
             }
 
         }
     });
 
-    console.log("does not wait")
-
-    //this is when the server sends a message to socket "client message"
+    //message from server
     socket.on('client message', data => {
         const li = document.createElement('li');
-        li.innerHTML = `Your mesage is: ${data.serverOut}`; //data.serverOut grams that json key "serverOUt"
+        li.innerHTML = `Your mesage is: ${data.serverOut}`; 
         document.querySelector('#messages').append(li);
     });
 
+    //whenever a new user enters a room update the current users list  with handle bars template
     socket.on('new user', data => {
-        console.log("new user is "+data.newUser)
-        console.log("current user is  "+localStorage.getItem('userName'))
-        if (data.newUser!=localStorage.getItem('userName')) {
+        if (data.newUser != localStorage.getItem('userName')) {
             const post_template = Handlebars.compile(document.querySelector('#selectionTemplate').innerHTML);
-            const post = post_template({'content': data.newUser});
+            const post = post_template({ 'content': data.newUser });
             document.querySelector('#channelUsers').innerHTML += post;
-
             createButtons();
         }
-
-
     });
 
 
 });
+
+
+function setAttributes() {
+    document.querySelector('span').innerHTML = `Hi, ${localStorage.getItem('userName')}`;
+    localStorage.setItem('lastChannel', document.URL);
+    const request = new XMLHttpRequest();
+    request.open('POST', location.pathname);
+    const data = new FormData();
+    data.append('userName', localStorage.getItem('userName'));
+    request.send(data);
+}
+
+function createButtons() {
+    document.querySelectorAll('.currentUsers').forEach(button => {
+        button.onclick = () => {
+            recepient = button.dataset.user;
+            console.log(recepient);
+        };
+    });
+}
