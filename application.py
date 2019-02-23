d2a11bd1e37c794eb77da95ac72c972f9c276be2
Oscar <NAge@email.com>
2019-02-22 18:54:33 -0600

    Code refactoring by IDE.

--- application.py
+++ application.py
@@ -8,10 +8,12 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
 # Global Variables
-channel_list = set() 
-message={} #a map that links a channel name to a Queue ADT
-channel_users={}# for every channel associate with it a set that will have all current users
-users_sid={}#allows private messages by linking session ID of the connection to the respective user
+channel_list = set()
+message = {}  # a map that links a channel name to a Queue ADT
+channel_users = {}  # for every channel associate with it a set that will have all current users
+users_sid = {}  # allows private messages by linking session ID of the connection to the respective user
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -23,11 +25,11 @@ def create_join():
     return render_template("main.html", channel_list=channel_list)
 
 
-def add_user_channel(name,userName):
+def add_user_channel(name, userName):
     if name in channel_users:
         channel_users.get(name).add(userName)
     else:
-        channel_users[name]=set([userName])
+        channel_users[name] = set([userName])
 
 
 # iff the user clicks on the link, then the server will save the channel
@@ -35,48 +37,49 @@ def add_user_channel(name,userName):
 def channel(name):
     if request.method == "POST":
         userName = request.form.get("userName")
-        add_user_channel(name,userName)
+        add_user_channel(name, userName)
         return 'ok'
 
     if name not in channel_list:
         channel_list.add(name)
-        #associate with every channel a queue where messages will be stored
-        message[f'/channel/{name}']=Queue(4)    
+        # associate with every channel a queue where messages will be stored
+        message[f'/channel/{name}'] = Queue(100)
 
-    current_Q=message.get(f'/channel/{name}')
-    return render_template("channel.html", channel_name=name,stored_messages=list(current_Q.queue),users=channel_users.get(name))
+    current_Q = message.get(f'/channel/{name}')
+    return render_template("channel.html", channel_name=name, stored_messages=list(current_Q.queue),
+                           users=channel_users.get(name))
 
 
 # this will accept a json object
 @socketio.on("server message")
 def process_message(data):
     selection = data["message"]
-    room=data["room"]
-    current_Q=message.get(room)
+    room = data["room"]
+    current_Q = message.get(room)
     if current_Q.full():
-        current_Q.get() #remove the oldest message
-        current_Q.put(selection) #insert a new message
+        current_Q.get()  # remove the oldest message
+        current_Q.put(selection)  # insert a new message
     else:
-       current_Q.put(selection) 
-    emit("client message", {"serverOut": selection,'test':'just a test'}, room=room)
+        current_Q.put(selection)
+    emit("client message", {"serverOut": selection, 'test': 'just a test'}, room=room)
 
 
 @socketio.on("private message")
 def private_message(data):
     selection = data["message"]
-    room=users_sid.get(data['recepient']) #get the sid of the recepient
-    room2=users_sid.get(data['sender'])
+    room = users_sid.get(data['recepient'])  # get the sid of the recepient
+    room2 = users_sid.get(data['sender'])
     emit("client message", {"serverOut": selection}, room=room)
     emit("client message", {"serverOut": selection}, room=room2)
 
 
-
 @socketio.on('join')
 def on_join(data):
-    users_sid[data['user']]=request.sid
-    room=data["room"]
+    users_sid[data['user']] = request.sid
+    room = data["room"]
     join_room(room)
     emit("new user", {"newUser": data['user']}, room=room)
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
